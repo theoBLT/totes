106e8a080ed743fa9919cb162d78e1d2e9823aea
[theoBLT] <[theobloc@gmail.com]>
2020-04-25 12:03:51 +0100

    Installed Stripe node package

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
+    const { STRIPE_TEST_API_KEY } = process.env;
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: `Stripe test API key is ${STRIPE_TEST_API_KEY}`
     });
 }
