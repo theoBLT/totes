a8689fe6309aeb83457ef054c1184d713ae6e46e
[theoBLT] <[theobloc@gmail.com]>
2020-04-25 14:27:11 +0100

    [Fix] Other payment intent creation approach

--- functions/paymentIntent.js
+++ functions/paymentIntent.js
@@ -2,21 +2,21 @@ exports.handler = function(event, context, callback) {
     const { STRIPE_TEST_API_KEY } = process.env;
 
     const stripe = require('stripe')(STRIPE_TEST_API_KEY);
-    const getServerSidePropos = async () => {
-
-        const paymentIntent = await stripe.paymentIntents.create({
-            payment_method_types: ['fpx'],
-            amount: 1099,
-            currency: 'myr',
-          });
-
-          return paymentIntent
-
-    }
-      
     
+    stripe.paymentIntents.create(
+        {
+          amount: 2000,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        },
+        function(err, paymentIntent) {
+          // asynchronously called
+          console.log(JSON.stringify(paymentIntent))
+        }
+      );
+
     callback(null, {
     statusCode: 200,
-    body: JSON.stringify(paymentIntent),
+    body: `Nothing to see here`,
     });
 }
