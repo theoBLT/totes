82b76a1826ecd46fca37b884b56565b501f1e0c3
[theoBLT] <[theobloc@gmail.com]>
2020-04-25 12:26:56 +0100

    [Fix] Put await inside async function

--- functions/paymentIntent.js
+++ functions/paymentIntent.js
@@ -2,17 +2,21 @@ exports.handler = function(event, context, callback) {
     const { STRIPE_TEST_API_KEY } = process.env;
 
     const stripe = require('stripe')(STRIPE_TEST_API_KEY);
+    const getServerSidePropos = async () => {
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        payment_method_types: ['fpx'],
-        amount: 1099,
-        currency: 'myr',
-      });
+        const paymentIntent = await stripe.paymentIntents.create({
+            payment_method_types: ['fpx'],
+            amount: 1099,
+            currency: 'myr',
+          });
+
+          return paymentIntent
+
+    }
       
-      console.log(paymentIntent);
     
     callback(null, {
     statusCode: 200,
-    body: `Stripe test API key is ${STRIPE_TEST_API_KEY}`
+    body: JSON.stringify(paymentIntent),
     });
 }
