21e7c1d8d72a25fc4155f0a75eeb00f0310572fe
[theoBLT] <[theobloc@gmail.com]>
2020-04-26 11:08:48 +0100

    feat - created async paymentIntent function

---
+++ functions/async-payment-intent.js
@@ -0,0 +1,27 @@
+/*
+*
+* This function creates a payment intent for an item
+* @see https://stripe.com/docs/api/payment_intents?lang=node
+*
+*/
+
+const stripe = require('stripe')(process.env.STRIPE_TEST_API_KEY);
+
+exports.handler = async (event) =>{
+
+    const paymentIntent = await stripe.paymentIntents.create(
+        {
+          amount: 2000,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        })
+
+    console.log(paymentIntent)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(paymentIntent)
+    };
+
+}
+
